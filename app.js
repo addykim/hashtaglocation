65ff251d393f86b1134eebf3d83d7ea2e6d43613
Addy Kim <addysgkim@gmail.com>
2017-04-01 22:25:47 -0500

    Add bootstrap cdn

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
