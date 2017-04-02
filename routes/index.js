46557e34feff578065b1c31bdbf1cae4881632b0
Addy Kim <addysgkim@gmail.com>
2017-04-02 01:14:21 -0500

    Instagram API call

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const request = require('request-promise')
 const router = express.Router();
 
 const CLIENT = require('../config.json')
-console.log(CLIENT)
 
-const INSTAGRAM_API = 'https://api.instagram.com/v1/'
+const INSTAGRAM_API = 'https://api.instagram.com/v1'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,19 +13,37 @@ router.get('/', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
   const REDIRECT_URI = 'http://localhost:3000/authorize'
-  const AUTHORIZE_URL = 'https://api.instagram.com/oauth/authorize/?client_id=' + CLIENT.IG_CLIENT_ID + '&redirect_uri=' + REDIRECT_URI + '&response_type=code'
+  const AUTHORIZE_URL = 'https://api.instagram.com/oauth/authorize/?client_id=' + CLIENT.IG_CLIENT_ID + '&redirect_uri=' + REDIRECT_URI + '&response_type=code&scope=public_content'
   res.redirect(AUTHORIZE_URL)
 })
 
 router.get('/authorize', function(req, res, next) {
-  const code = req.param('code')
+  // const code = req.params.code
+  // const REQUEST_ACCESS_TOKEN_URL = 'https://api.instagram.com/oauth/access_token'
+  // let options = {
+  //   url: REQUEST_ACCESS_TOKEN_URL,  
+
+  // }
+  // request(options, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //       // Print out the response body
+  //       console.log(body)
+  //   } else {
+  //     console.error(error)
+  //   }
+  // })
+  // res.cookie('access_token', code).render('index')
   res.redirect('http://localhost:3000')
 })
 
 router.post('/search', function(req, res, next) {
   let hashtags = stringToArray(req.body.hashtagInput)
   let location = req.body.locationInput
-  res.render('index')
+
+  let toEmbed = queryIGHashtag(hashtags[0])
+  console.log(toEmbed )
+  res.render('index', {frames: toEmbed})
+
 })
 
 // returns string if can't be done
@@ -36,10 +54,26 @@ function stringToArray(str) {
   return str
 }
 
-function queryIGHashTag(hashtag) {
-  const REQUEST_URL = INSTAGRAM_API + '/tags/' + hashtag + '?access_token=' + CLIENT.ACCESS_TOKEN
+function queryIGHashtag(hashtag) {
+  const REQUEST_URL = INSTAGRAM_API + '/tags/' + hashtag + '/media/recent?access_token=' + CLIENT.ACCESS_TOKEN
+  // Configure the request
+  var options = {
+      url: REQUEST_URL,
+      method: 'GET',
+      // client_id: CLIENT.IG_CLIENT_ID,
+      // client_secret: CLIENT.IG_CLIENT_SECRET,              
+      headers: {
+        'Content-Type': 'application/json'
+      }
+  }
 
-
+  request(options)
+    .then(function(response) {
+      // console.log(response)
+      return response
+    }).catch(function (err) {
+      console.error(err)
+    })
 }
 
 function queryIGLocation(location) {
